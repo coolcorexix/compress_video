020f4253fa76d304fb755cf6ce93618f509020a5
Phat Intern <coolcore.xix@gmail.com>
2024-12-11 12:17:58 +0700

    update installer

--- install_compress_video.sh
+++ install_compress_video.sh
@@ -26,9 +26,15 @@ if ! which ffmpeg &> /dev/null; then
     fi
 fi
 
+
+
 # Copy the compress_video script to /usr/local/bin
 curl -o compress_video.sh https://raw.githubusercontent.com/coolcorexix/compress_video/refs/heads/main/compress_video.sh
 echo "Installing compress_video script..."
+curl -o compress_video_quick_action.zip https://raw.githubusercontent.com/coolcorexix/compress_video/refs/heads/main/workflows/compress_video.workflow.zip
+unzip compress_video_quick_action.zip
+
+cp -R compress_video.workflow ~/Library/Services
 sudo cp compress_video.sh /usr/local/bin/compress_video
 
 
@@ -38,3 +44,5 @@ echo "Installation complete! You can now use the 'compress_video' command."
 echo "Usage: compress_video <input_file>" 
 # Remove the downloaded script
 rm compress_video.sh
+rm compress_video_quick_action.zip
+rm -rf compress_video.workflow
